ead8fe0f11f7f57a61e7f0defa6ce56bdf979596
kachilala <kachilala23@gmail.com>
2021-01-06 16:42:27 +0100

    refactored to use redux and react-redux

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Counter extends React.Component {
-  state = {
-    count: 0
-  };
-
+ 
   increment = () => {
-    this.setState(state => ({
-      count: state.count + 1
-    }));
+    this.props.dispatch({ type: 'INCREMENT'})
   };
 
   decrement = () => {
-    this.setState(state => ({
-      count: state.count - 1
-    }));
+    this.props.dispatch({ type: 'DECREMENT' })
   };
 
   render() {
@@ -23,7 +17,7 @@ class Counter extends React.Component {
         <h2>Counter</h2>
         <div>
           <button onClick={this.decrement}>-</button>
-          <span className="count">{this.state.count}</span>
+          <span className="count">{this.props.count}</span>
           <button onClick={this.increment}>+</button>
         </div>
       </div>
@@ -31,4 +25,10 @@ class Counter extends React.Component {
   }
 }
 
-export default Counter;
+const mapState = (state => {
+    return {
+        count: state.count
+    };
+});
+
+export default connect(mapState)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 
+const initialState =  {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count:  state.count + 1
+      };
+    case 'DECREMENT': 
+    return {
+      count: state.count -1
+    };
+    default:
+      return state; 
+  }
+  
+}
+
+const store = createStore(reducer);
+store.dispatch( { type: "INCREMENT" });
+
+
 const App = () => 
-  <div>
+  <Provider store={store}>
     <Counter />
-  </div>;
+  </Provider>;
 
 ReactDOM.render(
   <React.StrictMode>
